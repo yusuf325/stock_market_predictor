b02e11f25f8039efa6f92edbdfa901497f36e5ef
yusuf <yusuf_2002@hotmail.co.uk>
2023-10-11 14:26:24 +0100

    plotted exponential average predictions

--- prediction.py
+++ prediction.py
@@ -56,6 +56,36 @@ plt.show()
 
 
 
+window_size = 100
+N = train_mid.size
+
+run_avg_predictions = []
+run_avg_x = []
+
+mse_errors = []
+
+running_mean = 0.0
+run_avg_predictions.append(running_mean)
+
+decay = 0.5
+
+for pred_idx in range(1,N):
+
+    running_mean = running_mean*decay + (1.0-decay)*train_mid[pred_idx-1]
+    run_avg_predictions.append(running_mean)
+    mse_errors.append((run_avg_predictions[-1]-train_mid[pred_idx])**2)
+    run_avg_x.append(date)
+
+print('MSE error for EMA averaging: %.5f'%(0.5*np.mean(mse_errors)))
+
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),mid,color='b',label='True')
+plt.plot(range(0,N),run_avg_predictions,color='orange', label='Prediction')
+#plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+plt.xlabel('Date')
+plt.ylabel('Mid Price')
+plt.legend(fontsize=18)
+plt.show()
 
 
 
