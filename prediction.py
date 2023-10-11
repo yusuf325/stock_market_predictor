8a6cec2b5c07d9f9fcab352e1b39aea68e3f89e3
yusuf <yusuf_2002@hotmail.co.uk>
2023-10-11 14:25:54 +0100

    plotted standard average predictions

--- prediction.py
+++ prediction.py
@@ -30,6 +30,34 @@ scaled_train_mid = scaler.fit_transform(train_mid)
 
 
 
+window_size = 100
+N = train_mid.size
+std_avg_predictions = []
+std_avg_x = []
+mse_errors = []
+
+for pred_idx in range(window_size,N):
+    
+    date = df.loc[pred_idx,'Date']
+
+    std_avg_predictions.append(np.mean(train_mid[pred_idx-window_size:pred_idx]))
+    mse_errors.append((std_avg_predictions[-1]-train_mid[pred_idx])**2)
+    std_avg_x.append(date)
+
+print('MSE error for standard averaging: %.5f'%(0.5*np.mean(mse_errors)))
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),mid,color='b',label='True')
+plt.plot(range(window_size,N),std_avg_predictions,color='orange',label='Prediction')
+#plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+plt.xlabel('Date')
+plt.ylabel('Mid Price')
+plt.legend(fontsize=18)
+plt.show()
+
+
+
+
+
 
 
 
